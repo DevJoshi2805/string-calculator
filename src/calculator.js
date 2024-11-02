804041aec8d30bf0b7b2f9bff18212c429d1f13f
Devendra Joshi <Devdex1@gmail.com>
2024-11-02 16:32:03 +0530

    Handled Extra cases from TDD page

--- src/calculator.js
+++ src/calculator.js
@@ -1,21 +1,35 @@
 import React from 'react'
 
-
 const add = (numbers) => {
     if(!numbers) return 0;
 
     const delimiters = [',', '\n']
-    var customDelimiters = numbers.match(/^\/\/(.)\n/)
-    if (customDelimiters) {
-        delimiters.push(customDelimiters[1])
-        numbers = numbers.slice(4)
+    var customDelimiters 
+    // = numbers.match(/^\/\/(.*?)\n/)
+    // if (customDelimiters) {
+    //     delimiters.push(customDelimiters[1])
+    //     numbers = numbers.slice(4)
+    // }
+
+    if (numbers.startsWith("//")) {
+        numbers = numbers.slice(2)
+        var customPatters = numbers.split("\n")[0]
+        numbers = numbers.substring(customPatters.length + 1)
+        
+        var customDelimiters = customPatters.match(/\[(.*?)\]/g)
+        if (customDelimiters) {
+            customDelimiters.forEach(d => delimiters.push(d.slice(1,-1)) )
+        } else {
+            delimiters.push(customPatters)
+        }
     }
     
     var numbersList = numbers.split(new RegExp(`[${delimiters.join("")}]`)).filter(Boolean).map((number) => parseInt(number))
+    
     var negativeNumbers = numbersList.filter((number) => number < 0)
     if (negativeNumbers.length > 0) throw new Error(`negative numbers not allowed ${negativeNumbers.join(",")}`)
-    
-    return numbersList.reduce((sum, number) => sum + number)
+
+    return numbersList.filter((number) => number <= 1000).reduce((sum, number) => sum + number)
 }
 
 export default add
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -27,7 +27,22 @@ describe('String Calculator', () => {
     test(`returns negative numbers not allowed -1,-2 for string '-1,-2,5' `, () => { 
         expect(() => add('-1,-2,5')).toThrow("negative numbers not allowed -1,-2");
     })
-    test('return 1 for string 1,', () => {
+    test(`return 1 for string '1,'`, () => {
         expect(add('1,')).toBe(1);
+    })
+    test(`return 2 for string '2,1001'`, () => {
+        expect(add('2,1001')).toBe(2)
+    })
+    test(`returns 3 for string "//[;;]\n1;;2" `, () => { 
+        expect(add("//[;;]\n1;;2")).toBe(3);
+    })
+    test(`returns 6 for string "//[;][*]\n1;2*3" `, () => { 
+        expect(add("//[;][*]\n1;2*3")).toBe(6);
+    })
+    test(`returns 6 for string "//[;;;][***]\n1;;;2***3" `, () => { 
+        expect(add("//[;;;][***]\n1;;;2***3")).toBe(6);
+    })
+    test(`returns 6 for string "//[;;;][**]\n1;;;2**3" `, () => { 
+        expect(add("//[;;;][**]\n1;;;2**3")).toBe(6);
     })
 })
